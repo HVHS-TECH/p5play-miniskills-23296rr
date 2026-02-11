453a579b76452cb79e31554b3e92c73036b820f5
Remy Robert <23296rr@hvhs.school.nz>
2026-02-12 10:59:53 +1300

    3

--- t05_createWalls.js
+++ t05_createWalls.js
@@ -16,19 +16,35 @@ function setup() {
 	cnv = new Canvas(windowWidth, windowHeight);
 
 	ball_1 = new Sprite(width/2, height/2, 50, 'd');
-	ball_1.color = 'cyan';
-	ball_1.vel.x = 2;
+	ball_1.color = 'grey';
 	ball_1.bounciness = 1;
-	ball_1.friction = 0;
-	ball_1.drag = 0;
-	
-	wallLH  = new Sprite(0, 0, 8, windowHeight, 'k');
-	wallLH.color = 'black';
 
-	box4 = new Sprite (900, 1000, 150, 20, 'k')
-	box4.color = '#5656';
-	box4.rotation = 0.1
-	box4.bounciness = 1.075
+	ball_2 = new Sprite(300, height/2, 50, 'd');
+	ball_2.color = 'white';
+	ball_2.bounciness = 1;
+	ball_2.drag = 1;
+
+	ball_3 = new Sprite(1600, height/2, 50, 'd');
+	ball_3.color = 'black';
+	ball_3.bounciness = 1;
+	ball_3.drag = -0.4;
+
+	
+	wallLeft  = new Sprite(100, 500, 8, windowHeight, 'k');
+	wallLeft.color = 'black';
+	wallLeft.rotation = 0;
+
+	floor  = new Sprite(1000, 900, windowWidth, 8, 'k');
+	floor.color = 'red';
+	floor.rotation = 0;
+
+	wallRight  = new Sprite(1800, 500, 8, windowHeight, 'k');
+	wallRight.color = 'blue';
+	wallRight.rotation = 0;
+
+	roof  = new Sprite(1000, 100, windowWidth, 8, 'k');
+	roof.color = 'yellow';
+	roof.rotation = 0;
 }
 	
 /*******************************************************/
--- t06_createAliens.js
+++ t06_createAliens.js
@@ -1,22 +1,57 @@
 /*******************************************************/
-// P5.play: t06_createAliens
-// Create aliens on mouse click
-// Written by ???
+// P5.play: t01_create_sprite
+// Create a sprite
+/// Written by ???
 /*******************************************************/
 	
 /*******************************************************/
 // setup()
+
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
 
+	world.gravity.y = 10;
+
+	cnv = new Canvas(windowWidth, windowHeight);
+
+	ball_1 = new Sprite(width/2, height/2, 50, 'd');
+	ball_1.color = 'grey';
+	ball_1.bounciness = 1;
+
+	for (i = 0; i < ???; i++) {
+		alien = new Sprite(...);
+		alien.vel.x = 3;
+		alien.vel.y = 4;
+		alien.bounciness = 1;
+		alien.friction = 0;
+	}
+}
+
+
+function fromSetup() {
+	wallLeft  = new Sprite(100, 500, 8, windowHeight, 'k');
+	wallLeft.color = 'black';
+	wallLeft.rotation = 0;
+
+	floor  = new Sprite(1000, 900, windowWidth, 8, 'k');
+	floor.color = 'red';
+	floor.rotation = 0;
+
+	wallRight  = new Sprite(1800, 500, 8, windowHeight, 'k');
+	wallRight.color = 'blue';
+	wallRight.rotation = 0;
+
+	roof  = new Sprite(1000, 100, windowWidth, 8, 'k');
+	roof.color = 'yellow';
+	roof.rotation = 0;
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('243')
 }
 
 /*******************************************************/
